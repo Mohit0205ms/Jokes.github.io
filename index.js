e5caeaa999f9c2b14bef8e79809e14567a40dee9
Mohit1124 <100281071+Mohit1124@users.noreply.github.com>
2023-08-31 21:29:00 +0530

    deploy

---
+++ index.js
@@ -0,0 +1,12 @@
+const jokeContainer=document.getElementById("joke");
+const btn=document.getElementById("btn");
+const url="https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,sexist&type=single";
+let getJoke = () =>{
+    fetch(url)
+    .then( data => data.json())
+    .then(item => {
+        jokeContainer.textContent=`${item.joke}`;
+    });
+}
+btn.addEventListener('click',getJoke);
+getJoke();
